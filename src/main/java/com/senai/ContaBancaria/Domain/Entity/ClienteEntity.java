4eb27fc4a9c6a1f20ff0c7fdfbce8177a0866018
therciane <vasconcelostherciane@gmail.com>
2025-09-10 11:15:49 -0300

    Entidade conta feito

--- src/main/java/com/senai/ContaBancaria/Domain/Entity/ClienteEntity.java
+++ src/main/java/com/senai/ContaBancaria/Domain/Entity/ClienteEntity.java
@@ -1,4 +1,30 @@
 package com.senai.ContaBancaria.Domain.Entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class ClienteEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Size(max = 5)
+    private String id;
+
+    @NotBlank(message = "Está area não pode ficar em branco")
+    @Size(min = 3, max = 80)
+    private String nomeCompleto;
+
+    @NotNull(message = "Area Obrigatoria")
+    @Column(nullable = false, length = 11)
+    private long cpf;
+
+    @NotBlank(message = "Area que indica qual a sua conta")
+    private String tipoContas;
 }
