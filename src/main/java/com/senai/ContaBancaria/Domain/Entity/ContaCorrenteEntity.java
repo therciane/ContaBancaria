d1ccfdb1dba0241c9d411f07a23e501e108878a8
therciane <vasconcelostherciane@gmail.com>
2025-09-12 09:28:16 -0300

    Classe ContaCorrenteEntity abstratada

--- src/main/java/com/senai/ContaBancaria/Domain/Entity/ContaCorrenteEntity.java
+++ src/main/java/com/senai/ContaBancaria/Domain/Entity/ContaCorrenteEntity.java
@@ -1,6 +1,17 @@
 package com.senai.ContaBancaria.Domain.Entity;
 
-public class ContaCorrenteEntity {
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.validator.constraints.Length;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+public class ContaCorrenteEntity extends ContaEntity{
+    @NotNull
+    @Length(min = 1)
     private double limite;
 
     private long taxa;
