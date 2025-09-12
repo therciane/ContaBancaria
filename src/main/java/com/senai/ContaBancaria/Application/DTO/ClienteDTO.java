95ee75cd8eada51e44833a49a730da7432669d88
therciane <vasconcelostherciane@gmail.com>
2025-09-12 09:02:16 -0300

    clienteDTO feito.

--- src/main/java/com/senai/ContaBancaria/Application/DTO/ClienteDTO.java
+++ src/main/java/com/senai/ContaBancaria/Application/DTO/ClienteDTO.java
@@ -1,5 +1,24 @@
 package com.senai.ContaBancaria.Application.DTO;
 
-public record ClienteDTO() {
+import com.senai.ContaBancaria.Domain.Entity.ClienteEntity;
 
+import java.util.List;
+
+public record ClienteDTO(
+        String id,
+        String nomeCompleto,
+        long cpf,
+        List<String> tipContas
+) {
+
+    public static ClienteDTO fromEntity(ClienteEntity cliente){
+        if (cliente == null) return null;
+
+        return new ClienteDTO(
+                cliente.getId(),
+                cliente.getNomeCompleto(),
+                cliente.getCpf(),
+                cliente.getTipoContas()
+        );
+    }
 }
