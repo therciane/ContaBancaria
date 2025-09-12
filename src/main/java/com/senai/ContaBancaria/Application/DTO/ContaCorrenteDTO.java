4e678874840b74c350a8a3a3a9eb473cb8bacf2d
therciane <vasconcelostherciane@gmail.com>
2025-09-12 09:14:39 -0300

    ContaCorrenteDTO feito.

--- src/main/java/com/senai/ContaBancaria/Application/DTO/ContaCorrenteDTO.java
+++ src/main/java/com/senai/ContaBancaria/Application/DTO/ContaCorrenteDTO.java
@@ -1,4 +1,30 @@
 package com.senai.ContaBancaria.Application.DTO;
 
-public record ContaCorrenteDTO() {
+import com.senai.ContaBancaria.Domain.Entity.ContaCorrenteEntity;
+
+public record ContaCorrenteDTO(
+        int numeroConta,
+        double saldo,
+        double limite,
+        long taxa
+) {
+
+    public static ContaCorrenteDTO fromEntity(ContaCorrenteEntity contaCorrente){
+        if (contaCorrente == null) return null;
+        return new ContaCorrenteDTO(
+                contaCorrente.getNumeroConta(),
+                contaCorrente.getSaldo(),
+                contaCorrente.getLimite(),
+                contaCorrente.getTaxa()
+        );
+    }
+
+    public ContaCorrenteEntity toEntity(){
+        ContaCorrenteEntity contaCorrente = new ContaCorrenteEntity();
+        contaCorrente.setNumeroConta(this.numeroConta);
+        contaCorrente.setSaldo(this.saldo);
+        contaCorrente.setLimite(this.limite);
+        contaCorrente.setTaxa(this.taxa);
+        return contaCorrente;
+    }
 }
