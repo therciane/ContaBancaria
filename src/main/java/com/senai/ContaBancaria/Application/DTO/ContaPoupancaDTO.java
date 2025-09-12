9b075c25b5112c4d79f05eed17ad3bbd6971faff
therciane <vasconcelostherciane@gmail.com>
2025-09-12 10:03:29 -0300

    Classe ContaPoupancaDTO feito

--- src/main/java/com/senai/ContaBancaria/Application/DTO/ContaPoupancaDTO.java
+++ src/main/java/com/senai/ContaBancaria/Application/DTO/ContaPoupancaDTO.java
@@ -1,4 +1,30 @@
 package com.senai.ContaBancaria.Application.DTO;
 
-public record ContaPoupancaDTO() {
+import com.senai.ContaBancaria.Domain.Entity.ContaEntity;
+import com.senai.ContaBancaria.Domain.Entity.ContaPoupancaEntity;
+
+public record ContaPoupancaDTO(
+        String id,
+        int numeroConta,
+        double saldo,
+        long rendimento
+) {
+
+    public static ContaPoupancaDTO fromEntity(ContaPoupancaEntity contaPoupanca){
+        if (contaPoupanca == null) return null;
+        return new ContaPoupancaDTO(
+                contaPoupanca.getId(),
+                contaPoupanca.getNumeroConta(),
+                contaPoupanca.getSaldo(),
+                contaPoupanca.getRendimento()
+        );
+    }
+
+    public ContaPoupancaEntity toEntity(){
+        ContaPoupancaEntity contaPoupanca = new ContaPoupancaEntity();
+        contaPoupanca.setNumeroConta(this.numeroConta);
+        contaPoupanca.setSaldo(this.saldo);
+        contaPoupanca.setRendimento(this.rendimento);
+        return contaPoupanca;
+    }
 }
