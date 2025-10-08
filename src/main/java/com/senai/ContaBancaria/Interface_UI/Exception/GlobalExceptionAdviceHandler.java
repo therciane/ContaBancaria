62125b48460937d8703066a93db61c637e1b2491
therciane <vasconcelostherciane@gmail.com>
2025-10-08 10:17:10 -0300

    Handler feito

--- src/main/java/com/senai/ContaBancaria/Interface_UI/Exception/GlobalExceptionAdviceHandler.java
+++ src/main/java/com/senai/ContaBancaria/Interface_UI/Exception/GlobalExceptionAdviceHandler.java
@@ -1,6 +1,6 @@
 package com.senai.ContaBancaria.Interface_UI.Exception;
 
-import com.senai.ContaBancaria.Domain.Exceptions.ValoresNegativosException;
+import com.senai.ContaBancaria.Domain.Exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,12 +9,39 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionAdviceHandler {
 
-    //Ele trata a exceção ValoresNegativosException e retorna uma resposta HTTP com o status 400 (Bad Request) e a mensagem da exceção.
-    @ExceptionHandler(ValoresNegativosException.class)
-    public ResponseEntity<String> handleValoresNegativos (ValoresNegativosException ex) {
+    @ExceptionHandler(ContaMesmoTipoException.class)
+    public ResponseEntity<String> handleContaMesmoTipo(ContaMesmoTipoException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(EntidadeNaoEncontradaException.class)
+    public ResponseEntity<String> handleEntidadeNaoEncontrada(EntidadeNaoEncontradaException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(RendimentoInvalidoException.class)
+    public ResponseEntity<String> handleRendimentoInvalido(RendimentoInvalidoException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(SaldoInsuficienteException.class)
+    public ResponseEntity<String> handleSaldoInsuficiente(SaldoInsuficienteException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
 
+    @ExceptionHandler(TipoDeContaInvalidaException.class)
+    public ResponseEntity<String> handleTipoDeContaInvalida(TipoDeContaInvalidaException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 
+    @ExceptionHandler(TransferenciaMesmaContaException.class)
+    public ResponseEntity<String> handleTransferenciaParaMesmaConta(TransferenciaMesmaContaException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    //Ele trata a exceção ValoresNegativosException e retorna uma resposta HTTP com o status 400 (Bad Request) e a mensagem da exceção.
+    @ExceptionHandler(ValoresNegativosException.class)
+    public ResponseEntity<String> handleValoresNegativos(ValoresNegativosException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 }
