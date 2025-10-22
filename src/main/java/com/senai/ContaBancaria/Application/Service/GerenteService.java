b77b0918fb79a8dc725e513e51766bd03a5eae28
therciane <vasconcelostherciane@gmail.com>
2025-10-22 08:38:31 -0300

    Adição de dependencias no pom.xml, Service do gerente pronto e edição do Enum

---
+++ src/main/java/com/senai/ContaBancaria/Application/Service/GerenteService.java
@@ -0,0 +1,36 @@
+package com.senai.ContaBancaria.Application.Service;
+
+import com.senai.ContaBancaria.Application.DTO.GerenteDTO;
+import com.senai.ContaBancaria.Domain.Entity.GerenteEnity;
+import com.senai.ContaBancaria.Domain.Repository.GerenteRepository;
+import com.senai.ContaBancaria.Domain.Enum.Role;
+import lombok.RequiredArgsConstructor;
+import java.util.List;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GerenteService {
+    private final GerenteRepository gerenteRepository;
+
+    private final PasswordEncoder encoder;
+
+    @PreAuthorize("hasAnyRole('ADMIN','PROFESSOR')")
+    public List<GerenteDTO> listarTodosGerentes() {
+        return gerenteRepository.findAll().stream()
+                .map(GerenteDTO::fromEntity)
+                .toList();
+    }
+
+
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public GerenteDTO cadastrarProfessor(GerenteDTO dto) {
+        GerenteEnity entity = dto.toEntity();
+        entity.setSenha(encoder.encode(dto.senha()));
+        entity.setRole(Role.GERENTE);
+        gerenteRepository.save(entity);
+        return GerenteDTO.fromEntity(entity);
+    }
+}
--- src/main/java/com/senai/ContaBancaria/Domain/Enum/Role.java
+++ src/main/java/com/senai/ContaBancaria/Domain/Enum/Role.java
@@ -1,5 +1,5 @@
 package com.senai.ContaBancaria.Domain.Enum;
 
 public enum Role extends javax.management.relation.Role {
-   ADMIN, PROFESSOR
+   ADMIN, GERENTE
 }
