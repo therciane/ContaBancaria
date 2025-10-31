dba470bc319ed3514e7223c2f38ad7dab17e4ae2
therciane <vasconcelostherciane@gmail.com>
2025-10-31 08:54:58 -0300

    Criação da entidade TaxaEntity e realização da entidade PagamentoEntity

--- src/main/java/com/senai/ContaBancaria/Domain/Entity/PagamentoEntity.java
+++ src/main/java/com/senai/ContaBancaria/Domain/Entity/PagamentoEntity.java
@@ -1,5 +1,43 @@
 package com.senai.ContaBancaria.Domain.Entity;
 
-public class PagamentoEntity  {
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.SuperBuilder;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+@SuperBuilder
+@Table (name = "Pagamento")
+@DiscriminatorValue("PAGAMENTO")
+
+public class PagamentoEntity extends ContaEntity {
+    @NotNull
+    @Column (nullable = false, length = 48) //tamanho de um codigo de barras de um boleto comum no Brasil
+    private String boleto;
+
+    @Column (name = "valor_pago", precision = 15, scale = 2)
+    @Positive
+    @NotNull
+    private BigDecimal valorPago;
+
+    @Column (name = "data_pagamento")
+    @NotNull
+    private LocalDate dataPagamento;
+
+    @Column (nullable = false)
+    private Boolean status;
+
+    @Override
+    public String getTipoConta() {
+        return "PAGAMENTO";
+    }
 }
