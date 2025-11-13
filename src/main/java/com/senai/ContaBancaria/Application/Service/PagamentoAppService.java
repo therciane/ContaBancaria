ed47d83166a970db9bac700882640887e7afd28d
therciane <vasconcelostherciane@gmail.com>
2025-11-13 08:59:55 -0300

    Criação do repository de PagamentoRepository.java e inicio do AppService de PagamentoAppService.java

--- src/main/java/com/senai/ContaBancaria/Application/Service/PagamentoAppService.java
+++ src/main/java/com/senai/ContaBancaria/Application/Service/PagamentoAppService.java
@@ -1,7 +1,24 @@
 package com.senai.ContaBancaria.Application.Service;
 
+import com.senai.ContaBancaria.Application.DTO.PagamentoDTO;
+import com.senai.ContaBancaria.Domain.Repository.PagamentoRepository;
+import com.senai.ContaBancaria.Domain.Repository.ServicoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+
 public class PagamentoAppService {
 
+    private final PagamentoDTO dto;
+    private final PagamentoRepository repository;
 
+    public PagamentoAppService (PagamentoRepository repository, PagamentoDTO pgdto) {
+        this.repository = repository;
+        this.dto = pgdto;
+    }
 
 }
