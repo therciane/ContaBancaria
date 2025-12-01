175ea9eb06fa8a1f157f7f7a82ed2f20a188695e
therciane <vasconcelostherciane@gmail.com>
2025-12-01 08:46:42 -0300

    IoTMqttListener meio pronto

--- src/main/java/com/senai/ContaBancaria/Infrastructure/Rest/IoTMqttListener.java
+++ src/main/java/com/senai/ContaBancaria/Infrastructure/Rest/IoTMqttListener.java
@@ -1,5 +1,28 @@
 package com.senai.ContaBancaria.Infrastructure.Rest;
 
-public class IoTMqttListener {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.senai.ContaBancaria.Application.Service.AutenticacaoIotService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class IoTMqttListener {
+    private final AutenticacaoIotService authService;
+
+    @MqttSubscriber(topic = "banco/validacao/+")
+    public void onValidacao(String topic, String payload) throws JsonProcessingException {
+        String clienteId = topic.split("/")[2];
+
+        JsonNode json = new ObjectMapper().readTree(payload);
+
+        String codigo = json.get("codigo").asText();
+        boolean confirmado = json.get("confirmado").asBoolean();
+
+        authService.receberValidacao(UUID.fromString(clienteId), codigo, confirmado);
+    }
 }
